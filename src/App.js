6b8cba998c8f2d0f4ddca9a6a653254199a42f6f
AyaMouawi <ayamouawi8@gmail.com>
2023-12-27 23:49:37 +0200

    login register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import HomePage from './Components/Pages/HomePage';
 import { Routes,Route } from 'react-router-dom';
+import SignIn from './Components/Pages/SignIn';
+import SignUp from './Components/Pages/SignUp';
 
 function App() {
   return (
   <Routes>
      <Route path='/' element={ <HomePage/>}/>
-
+     <Route path='/SignIn' element={ <SignIn />}/>
+     <Route path='/SignUp' element={ <SignUp />}/>
     </Routes>
   );
 }
